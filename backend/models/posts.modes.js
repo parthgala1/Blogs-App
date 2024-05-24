8860a31bbdebb3ae699d34a7bf328f018a994df5
parthpsg <66634291+parthgala1@users.noreply.github.com>
2024-05-24 19:16:12 +0530

    Creating backend

---
+++ backend/models/posts.modes.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from "mongoose";
+
+const postSchema = new Schema(
+    {
+        title:{
+            type: String,
+            required: true,
+        },
+        content:{
+            type: String,
+            required: true,
+        },
+    }
+    , { timestamps: true });
+
+const Post = mongoose.model("Post", postSchema);
+export default Post;
